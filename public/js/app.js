1119a1d2096bd2f09897bb65a32008d6e703b974
trashymuta <trashymuta@wp.pl>
2019-03-20 10:45:17 +0100

    set up for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ weatherFoem.addEventListener('submit', (e) => {
     messageOne.textContent = ''
     messageTwo.textContent = ''
     messageThree.textContent = ''
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
--- src/app.js
+++ src/app.js
@@ -4,8 +4,9 @@ const hbs = require('hbs')
 const weather = require('./utils/weather.js')
 const geocode = require('./utils/geocode.js')
 
-
 const app = express()
+
+const port = process.env.PORT || 3000
 
 const publicDirPath = path.join(__dirname, '../public')
 const viewsDirPath = path.join(__dirname, '../templates/views')
@@ -78,8 +79,8 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('server is up on port 3000.')
+app.listen(port, () => {
+    console.log('server is up on port: ' + port)
 })
 
 // app.com
